495bfa2f551e44a93ac3698128444ce8a9ab5f73
Andres <andresmelo482@gmail.com>
2026-04-04 14:28:06 -0500

    se cambio nombre de EmpleadoVehiculosViewModel a EmpleadoReportesViewModel

---
+++ Models/ViewModels/EmpleadoReportesViewModel.cs
@@ -0,0 +1,39 @@
+using ParkYa.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ParkYa.ViewModels
+{
+   
+    public class EmpleadoReportesViewModel
+    {
+        public Usuario Empleado { get; set; } = new Usuario();
+
+       
+        public int TotalReservasHoy { get; set; }
+        public int TotalReservasSemana { get; set; }
+        public int TotalReservasMes { get; set; }
+
+     
+public List<ReservaItemViewModel> HistorialHoy { get; set; } = new();
+public List<ReservaItemViewModel> HistorialSemana { get; set; } = new();
+public List<ReservaItemViewModel> HistorialMes { get; set; } = new();
+        public int TotalReservas { get; set; }
+    }
+
+   
+    public class ReservaItemViewModel
+    {
+        public int IdReserva { get; set; }
+        public string Cliente { get; set; } = string.Empty;
+        public string Placa { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; }
+        public TimeSpan? HoraEntrada { get; set; }
+        public TimeSpan? HoraSalida { get; set; }
+        public string Estado { get; set; } = string.Empty;
+
+       
+        public TimeSpan? HoraReservada { get; set; }
+        public decimal? TarifaHora { get; set; }
+    }
+}
